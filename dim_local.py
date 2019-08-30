5d63cb95af9eddf14c6aba0cc34cac7b2161ba61
tanimu <tanimu@ht.sfc.keio.ac.jp>
2019-08-30 18:12:21 +0900

    Add ConvClassifier which combine encoder and classfier using conv feature

--- dim_local.py
+++ dim_local.py
@@ -3,31 +3,60 @@ import torch.nn as nn
 from collections import OrderedDict
 
 
-class Encoder(nn.Module):
-    def __init__(self, classifier):
+feature_types = ['conv', 'fc', 'glob']
+network_types = ['encoder', 'classifier']
+
+class ConvClassifier(nn.Module):
+    def __init__(self):
         super().__init__()
-        self.encoder = Convnet(classifier)
+        self.encoder = Convnet('conv_encoder')
+        self.classifier = Convnet('conv_classifier')
 
     def forward(self, x):
-        x = self.encoder(x)
-        return x
-
+        encoding = self.encoder(x)
+        out = self.classifier(encoding)
+        return out
 
 class Convnet(nn.Module):
-    def __init__(self, classifier):
+    def __init__(self, network):
         super().__init__()
-        
-        assert classifier in ['conv', 'fc', 'glob']
-        self.classifier = classifier
 
-        self.layers = nn.Sequential(OrderedDict([
-                            ('layer0', self.make_conv_layer(3, 64)),
-                            ('layer1', self.make_conv_layer(64, 128)),
-                            ('layer2', self.make_conv_layer(128, 256)),
-                            ('layer3', View(-1, 4096)),
-                            ('layer4', self.make_fc_layer(4096, 1024)),
-                            ('layer5', nn.Linear(1024, 64, bias=True))
-                            ]))
+        feature_type, network_type = network.split('_')
+        assert feature_type in feature_types
+        assert network_type in network_types
+
+        if network_type == 'encoder':
+            self.layers = self.make_encoder(feature_type)
+        else:
+            self.layers = self.make_classifier(feature_type)
+
+    def make_encoder(self, feature_type):
+        modules = nn.Sequential()
+        modules.add_module('layer0', self.make_conv_layer(3, 64))
+        modules.add_module('layer1', self.make_conv_layer(64, 128))
+        modules.add_module('layer2', self.make_conv_layer(128, 256))
+        if feature_type == 'conv':
+            return modules
+        modules.add_module('layer3', View(-1, 4096))
+        modules.add_module('layer4', self.make_fc_layer(4096, 1024))
+        if feature_type == 'fc':
+            return modules
+        modules.add_module('layer5', nn.Linear(1024, 64, bias=True))
+        return modules
+
+    def make_classifier(self, feature_type):
+        modules = nn.Sequential()
+        if feature_type == 'conv':
+            args = dict(in_f=4096, out_f=200, bias=True, 
+                        flatten=True, drop=0.1)
+        elif feature_type == 'fc':
+            args = dict(in_f=1024, out_f=200, bias=True, drop=0.1)
+        elif feature_type == 'glob':
+            args = dict(in_f=64, out_f=200, bias=True, drop=0.1)
+
+        modules.add_module('layer0', self.make_fc_layer(**args))
+        modules.add_module('layer1', nn.Linear(200, 10, bias=True))
+        return modules
 
     def make_conv_layer(self, in_c, out_c, kernel_size=(4, 4), stride=(2, 2),
                         padding=(1, 1), bias=False, eps=1e-05, momentum=0.1,
@@ -43,27 +72,25 @@ class Convnet(nn.Module):
                                           ('ReLU', relu)]))
 
     def make_fc_layer(self, in_f, out_f, bias=False, eps=1e-05, momentum=0.1,
-                      affine=True, track_running_stats=True, inplace=True):
-        fc = nn.Linear(in_f, out_f, bias=bias)
-        bn = nn.BatchNorm1d(out_f, eps=eps, momentum=momentum,
-                            affine=affine,
-                            track_running_stats=track_running_stats)
-        relu = nn.ReLU(inplace=inplace)
-        return nn.Sequential(OrderedDict([('fc', fc),
-                                          ('bn', bn),
-                                          ('ReLU', relu)]))
+                      affine=True, track_running_stats=True, inplace=True,
+                      flatten=False, drop=False):
+        modules = nn.Sequential()
+        if flatten:
+            modules.add_module('flatten', View(-1, in_f))
+        modules.add_module('fc', nn.Linear(in_f, out_f, bias=bias))
+        if drop:
+            modules.add_module('do', nn.Dropout(p=drop, inplace=False))
+        modules.add_module('bn', nn.BatchNorm1d(
+                    out_f, eps=eps, 
+                    momentum=momentum, affine=affine,
+                    track_running_stats=track_running_stats
+                    ))
+        modules.add_module('ReLU', nn.ReLU(inplace=inplace))
+        return modules
 
     def forward(self, x):
-        x = self.layers.layer0(x)
-        x = self.layers.layer1(x)
-        x = self.layers.layer2(x)
-        if self.classifier == 'conv':
-            return x
-        x = self.layers.layer3(x)
-        if self.classifier == 'fc':
-            return x
-        x = self.layers.layer4(x)
-        return x
+        out = self.layers(x)
+        return out
 
 
 class View(torch.nn.Module):
@@ -88,10 +115,17 @@ class View(torch.nn.Module):
 
 
 if __name__ == '__main__':
-    classifier = 'conv'
-    encoder = Encoder(classifier)
-    encoder.load_state_dict(torch.load('./models/dim_local_dv_encoder.pth'))
-    print(encoder)
+    model = ConvClassifier()
+    model.encoder.load_state_dict(
+            torch.load('./models/dim_local_dv_encoder.pth'),
+            strict=False
+            )
+    model.classifier.load_state_dict(
+            torch.load('./models/dim_local_dv_conv_classifier.pth'),
+            strict=False
+            )
+    model.eval()
+    print(model)
     # encoder = encoder.encoder
     # print(encoder)
 
@@ -101,8 +135,8 @@ if __name__ == '__main__':
     # # encoder.load_state_dict(torch.load('./models/dim_local_dv_encoder.pth'))
     # optimizer = optim.Adam(encoder.parameters())
     # optimizer.zero_grad()
-    x = torch.randn(1, 3, 32, 32, requires_grad=True)
-    out = encoder(x)
+    x = torch.randn(2, 3, 32, 32, requires_grad=True)
+    out = model(x)
     print(out.shape)
     # criterion = nn.L1Loss()
     # t = torch.ones(out.shape)
