1810ed571660d010ec80a4357664f93a3e9c5cf3
tanimu <tanimu@ht.sfc.keio.ac.jp>
2019-09-01 01:44:28 +0900

    Integrate all exec shell script to exec.sh

--- train_cifar.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
- 
-export LC_CTYPE=C.UTF-8
-docker-compose -f ./docker/docker-compose-gpu.yml run --rm -T experiment python3 scripts/main.py local classifier --d.source CIFAR10 -n DIM_CIFAR10 --t.epochs 1000 --t.no_ascii
