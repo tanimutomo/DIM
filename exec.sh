e2d34de6db5940480e3487fea60e2d82792c6a4c
tanimu <tanimu@ht.sfc.keio.ac.jp>
2019-09-01 16:51:41 +0900

    Arange exe.sh

--- exec.sh
+++ exec.sh
@@ -2,13 +2,40 @@
 
 export LC_CTYPE=C.UTF-8
 
-if [ "$1" = "train-cifar10" ]; then
-  docker-compose -f ./docker/docker-compose-gpu.yml run --rm -T experiment python3 scripts/main.py local classifier --d.source CIFAR10 -n DIM_CIFAR10 --t.epochs 1000 --t.no_ascii
+# main command
+docker="docker-compose -f ./docker/docker-compose-gpu.yml run --rm -T experiment"
+main_cmd="python3 scripts/main.py"
 
-elif [ "$1" = "val-cifar10" ]; then
-  docker-compose -f ./docker/docker-compose-gpu.yml run --rm -T experiment python3 scripts/main.py classifier --eval --d.source CIFAR10 -n DIM_CIFAR10_cls --t.eval_only -L .cortex/output/DIM_CIFAR10/binaries/DIM_CIFAR10_final.t7 --t.no_ascii
-
-elif [ "$1" = "dim-neg-local" ]; then
-  docker-compose -f ./docker/docker-compose-gpu.yml run --rm -T experiment python3 scripts/main.py glob local classifier --d.source CIFAR10 -n DIM_CIFAR10_neglocal --t.epochs 1000 --t.no_ascii --local.mode fd_neg
-
+if [ "$1" = "docker" ]; then
+  cmd="${docker} ${main_cmd}"
+else
+  cmd="${main_cmd}"
 fi
+
+
+# Basic options
+name="DIM_CIFAR10_$2"
+base_options="--d.source CIFAR10 --t.no_ascii --device $3 -n ${name}"
+
+
+# Other options
+if [ "$2" = "train-cifar10" ]; then
+  mode="local classifier"
+  options="--t.epochs 1000"
+
+elif [ "$2" = "val-cifar10" ]; then
+  mode="classifier"
+  options="--eval --t.eval_only -L ~/.cortex/output/DIM_CIFAR10/binaries/DIM_CIFAR10_final.t7"
+
+elif [ "$2" = "neg-local" ]; then
+  mode="glob local classifier"
+  options="--t.epochs 1000 --local.mode fd_neg"
+
+elif [ "$2" = "neg-half-local" ]; then
+  mode="glob local classifier"
+  options="--t.epochs 1000 --local.mode fd_neg --local.scale 0.5"
+fi
+
+
+# Execute command
+${cmd} ${mode} ${base_options} ${options}
