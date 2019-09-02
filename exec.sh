8f8be55537b4201be21d40567d3d6e9355f7c641
tanimu <tanimu@ht.sfc.keio.ac.jp>
2019-09-02 16:35:19 +0900

    Add evaluation method option  to exec.sh

--- exec.sh
+++ exec.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# $1 = docker or local
+# $2 = device id
+# $3 = name
+# $4 = evaluation method (optional)
+
 export LC_CTYPE=C.UTF-8
 
 # main command
@@ -14,24 +19,29 @@ fi
 
 
 # Basic options
-name="DIM_CIFAR10_$2"
-base_options="--d.source CIFAR10 --t.no_ascii --device $3 -n ${name}"
+if [ "$4" = "classification" ]; then
+  name="DIM_CIFAR10_cls_$3"
+else
+  name="DIM_CIFAR10_$3"
+fi
+base_options="--d.source CIFAR10 --t.no_ascii --device $2 -n ${name}"
 
 
 # Other options
-if [ "$2" = "train-cifar10" ]; then
+ckpt_path="$HOME/.cortex/ckpt"
+if [ "$4" = "classification" ]; then
+  mode="classifier"
+  options="--eval --t.epochs 1000 -L $ckpt_path/DIM_CIFAR10_$3/binaries/DIM_CIFAR10_$3_final.t7"
+
+elif [ "$3" = "default" ]; then
   mode="local classifier"
   options="--t.epochs 1000"
 
-elif [ "$2" = "val-cifar10" ]; then
-  mode="classifier"
-  options="--eval --t.eval_only -L ~/.cortex/output/DIM_CIFAR10/binaries/DIM_CIFAR10_final.t7"
-
-elif [ "$2" = "neg-local" ]; then
+elif [ "$3" = "neg-local" ]; then
   mode="glob local classifier"
   options="--t.epochs 1000 --local.mode fd_neg"
 
-elif [ "$2" = "neg-half-local" ]; then
+elif [ "$3" = "neg-half-local" ]; then
   mode="glob local classifier"
   options="--t.epochs 1000 --local.mode fd_neg --local.scale 0.5"
 
